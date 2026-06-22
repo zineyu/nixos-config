5e637648851993beb06455d44f6108d963f06a5e
zine yu <zine.xlws@gmail.com>
2026-06-22 16:09:31 +0800

    feat(home): 迁移常用 GUI 应用
    Signed-off-by: zine yu <zine.xlws@gmail.com>

--- modules/home/programs/default.nix
+++ modules/home/programs/default.nix
@@ -18,6 +18,24 @@
     ./fontconfig
     ./npm
     ./devtools
+    (
+      { config, pkgs, ... }:
+      let
+        wrapGui = config.lib.nixGL.wrap;
+      in
+      {
+        home.packages = with pkgs; [
+          (wrapGui localsend)
+          (wrapGui telegram-desktop)
+          (wrapGui qbittorrent)
+          (wrapGui vlc)
+          (wrapGui gimp)
+          (wrapGui dbeaver-bin)
+          (wrapGui thunderbird)
+          (wrapGui alacritty)
+        ];
+      }
+    )
     ./aria2
     ./zen-browser
     ./yay
