8cf9ce4de470ff30991541c4c9dd3cfced95b4db
zine yu <zine.xlws@gmail.com>
2026-07-13 13:35:55 +0800

    feat: install and configure Steam
    Signed-off-by: zine yu <zine.xlws@gmail.com>

--- hosts/tianxuan/configuration.nix
+++ hosts/tianxuan/configuration.nix
@@ -13,6 +13,7 @@
   imports = [
     # Include the results of the hardware scan.
     ./hardware-configuration.nix
+    ../../modules/nixos/steam.nix
   ];
 
   # Use the systemd-boot EFI boot loader.
---
+++ modules/nixos/steam.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+
+{
+  programs.steam = {
+    enable = true;
+    gamescopeSession.enable = true;
+    remotePlay.openFirewall = true;
+    dedicatedServer.openFirewall = true;
+    localNetworkGameTransfers.openFirewall = true;
+  };
+
+  # Steam and most games still ship 32-bit binaries, so 32-bit graphics
+  # drivers are required. The NVIDIA module wires up the matching 32-bit
+  # driver automatically when this is enabled.
+  hardware.graphics.enable32Bit = true;
+}
