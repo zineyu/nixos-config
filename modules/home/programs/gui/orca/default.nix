cfe388609f369101b1b09772b8eaeea9da561844
zine yu <zine.xlws@gmail.com>
2026-07-27 15:12:43 +0800

    feat(home): add Orca IDE (gui) via AppImage
    Package stablyai/orca 1.4.158 AppImage with appimageTools.wrapType2,
    installing the desktop entry and hicolor icons.
    
    Signed-off-by: zine yu <zine.xlws@gmail.com>

---
+++ modules/home/programs/gui/orca/default.nix
@@ -0,0 +1,36 @@
+{ pkgs, ... }:
+
+let
+  pname = "orca-ide";
+  version = "1.4.158";
+
+  src = pkgs.fetchurl {
+    url = "https://github.com/stablyai/orca/releases/download/v${version}/orca-linux.AppImage";
+    hash = "sha512-uuurGnuXqxX3skapuK25Gi72+B1WIgCAlBm4kvH/YTtbjgUn3m6Io5Fr3sUkh2wjdgvkXaSP/XgHLRFyhJKfEw==";
+  };
+
+  appimageContents = pkgs.appimageTools.extractType2 { inherit pname version src; };
+in
+{
+  home.packages = [
+    (pkgs.appimageTools.wrapType2 {
+      inherit pname version src;
+
+      extraInstallCommands = ''
+        install -Dm444 ${appimageContents}/orca-ide.desktop $out/share/applications/orca-ide.desktop
+        substituteInPlace $out/share/applications/orca-ide.desktop \
+          --replace-fail 'Exec=AppRun' 'Exec=orca-ide'
+        cp -r ${appimageContents}/usr/share/icons $out/share/
+      '';
+
+      meta = {
+        description = "Next-gen IDE for parallel agentic development";
+        homepage = "https://github.com/stablyai/orca";
+        license = pkgs.lib.licenses.mit;
+        sourceProvenance = with pkgs.lib.sourceTypes; [ binaryNativeCode ];
+        mainProgram = "orca-ide";
+        platforms = [ "x86_64-linux" ];
+      };
+    })
+  ];
+}
