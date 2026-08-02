a1895a053248a50afdb3a086bf893531784ecf49
zine yu <zine.xlws@gmail.com>
2026-08-02 17:05:06 +0800

    feat(nixos): enable nix gc, journal size limit and auto-optimise-store
    Signed-off-by: zine yu <zine.xlws@gmail.com>

--- modules/nixos/common/nix.nix
+++ modules/nixos/common/nix.nix
@@ -4,7 +4,20 @@ in
 {
   nix.settings = shared // {
     trusted-users = [ "zine" ];
+    auto-optimise-store = true;
   };
+
+  # 每周自动 GC，删除 30 天前的旧 generation
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  # 限制 systemd journal 日志体积
+  services.journald.extraConfig = ''
+    SystemMaxUse=1G
+  '';
 
   nixpkgs.config.allowUnfree = true;
 }
